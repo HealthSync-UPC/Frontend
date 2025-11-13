05543a2a5d80cf09f0ba04e6f8ea732eca05f313
Estrella Ticona <u202210659@upc.edu.pe>
2025-11-13 10:32:15 -0500

    fix: correct casing of firstname property to firstName in User model

--- src/settings/model/User.ts
+++ src/settings/model/User.ts
@@ -1,5 +1,5 @@
 export class User {
-    firstname: string;
+    firstName: string;
     lastName: string;
     email: string;
     qr: string;
@@ -7,7 +7,7 @@ export class User {
     position: string;
 
     constructor(firstname: string, lastName: string, email: string, qr: string, password: string, position: string) {
-        this.firstname = firstname;
+        this.firstName = firstname;
         this.lastName = lastName;
         this.email = email;
         this.qr = qr;
